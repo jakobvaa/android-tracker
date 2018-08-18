9913f3636cc1dfdce06f7346832559eaf4d167c1
jakobvaa <jakob.vaardal@gmail.com>
2018-08-18 15:39:33 +0200

    Added bottom-navigation, switches between fragments

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -3,13 +3,25 @@ package com.myapplication;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    FragmentTransaction ft;
+    Fragment currentFragment = null;
+
+    private static final String TAG = "MainActivity";
+
+    //private SectionStatePagerAdapter mSectionStatePagerAdapter;
+    //private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,16 +30,29 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    //mTextMessage.setText(R.string.title_home);
+                    Fragment1 fragment = new Fragment1();
+                    switchToFragment(fragment);
+                    break;
+
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    Fragment2 fragment2 = new Fragment2();
+                    switchToFragment(fragment2);
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    //mTextMessage.setText(R.string.title_notifications);
+                    Fragment3 fragment3 = new Fragment3();
+                    switchToFragment(fragment3);
+                    break;
+                    /**
+                case R.id.navigation_more:
+                    //mTextMessage.setText(R.string.title_more);
+                    switchToFragmeayout.fragment_onent3();
+                    break;
+                     **/
             }
-            return false;
+            return true;
         }
     };
 
@@ -36,9 +61,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        ft = getSupportFragmentManager().beginTransaction();
+        currentFragment = new Fragment1();
+        ft.replace(R.id.frame_layout, currentFragment);
+        ft.commit();
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public void switchToFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(currentFragment.getId(), fragment).commit();
+        currentFragment = fragment;
+    }
 }
